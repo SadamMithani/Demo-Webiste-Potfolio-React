49250edaf77f556dc9590b1e32a651e05b5637a8
SadamMithani <sadammithani92@gmail.com>
2023-01-10 18:59:13 +0530

    Updates

---
+++ precache-manifest.f7056e06391eec789c85b965ee8345b6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1939f63e37c3bc543df",
+    "url": "/Potfolio-React/static/css/main.b4cedb6e.chunk.css"
+  },
+  {
+    "revision": "e1939f63e37c3bc543df",
+    "url": "/Potfolio-React/static/js/main.e1939f63.chunk.js"
+  },
+  {
+    "revision": "e05535e9c6e43edb589d",
+    "url": "/Potfolio-React/static/css/1.3f924c71.chunk.css"
+  },
+  {
+    "revision": "e05535e9c6e43edb589d",
+    "url": "/Potfolio-React/static/js/1.e05535e9.chunk.js"
+  },
+  {
+    "revision": "ad4330dec56f0938c55e",
+    "url": "/Potfolio-React/static/js/runtime~main.ad4330de.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/Potfolio-React/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/Potfolio-React/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/Potfolio-React/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/Potfolio-React/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/Potfolio-React/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/Potfolio-React/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/Potfolio-React/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/Potfolio-React/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/Potfolio-React/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "787a45b3813a904ba36b8d9868b36c2c",
+    "url": "/Potfolio-React/index.html"
+  }
+];
